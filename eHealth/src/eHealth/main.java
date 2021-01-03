a999614833ed84cd4e792f5f65b47aaed2011e0e
marin <marinhincu1999@gmail.com>
2021-01-03 22:44:17 +0100

    quickAdd

--- eHealth/src/eHealth/main.java
+++ eHealth/src/eHealth/main.java
@@ -5,7 +5,8 @@ public class main {
 	public static void main(String[] args) {
 		
 		
-		//DBController.insertUserIntoDB("test1", "passwd", "person", "andaname", "1999-11-11", "sick", "someins", "public", "somestreet", 34, "234233", "frankfurt");
+		// EINMAL AUSFUEHREN DAMIT DIE NEUE TABELLE ERSTELLT WIRD
+		DBController.createUserDBTable();
 		
 		DBController.displayListOfAllDBEntries();
 		
