040d521b19c4d75b1a3133b7487b8d1e861c2ad8
jojo0899 <jobst_j@t-online.de>
2021-01-07 16:47:50 +0100

    Login Screen Design Change

--- eHealth/src/eHealth/LoginDialog.java
+++ eHealth/src/eHealth/LoginDialog.java
@@ -18,6 +18,8 @@ import java.awt.event.MouseEvent;
 import java.io.UnsupportedEncodingException;
 
 import static javax.swing.JOptionPane.*;
+import java.awt.Color;
+import java.awt.SystemColor;
 
 public class LoginDialog extends JFrame {
 
@@ -30,29 +32,34 @@ public class LoginDialog extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 500, 300);
 		contentPane = new JPanel();
+		contentPane.setBackground(SystemColor.textHighlight);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JLabel lblNewLabel_1 = new JLabel("eHealth Login");
-		lblNewLabel_1.setFont(new Font("Lucida Grande", Font.PLAIN, 35));
-		lblNewLabel_1.setBounds(113, 36, 297, 52);
+		lblNewLabel_1.setFont(new Font("Lucida Grande", Font.BOLD, 35));
+		lblNewLabel_1.setBounds(113, 36, 256, 52);
 		contentPane.add(lblNewLabel_1);
 		
 		JLabel usernamelbl = new JLabel("username:");
-		usernamelbl.setBounds(95, 125, 87, 16);
+		usernamelbl.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
+		usernamelbl.setBounds(95, 120, 87, 16);
 		contentPane.add(usernamelbl);
 		
 		JLabel lblPassword = new JLabel("password:");
+		lblPassword.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
 		lblPassword.setBounds(95, 153, 87, 16);
 		contentPane.add(lblPassword);
 		
 		usernameField = new JTextField();
-		usernameField.setBounds(194, 120, 175, 26);
+		usernameField.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
+		usernameField.setBounds(194, 115, 175, 26);
 		contentPane.add(usernameField);
 		usernameField.setColumns(10);
 		
 		passwordField = new JPasswordField();
+		passwordField.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
 		passwordField.setBounds(194, 148, 175, 26);
 		contentPane.add(passwordField);
 		
@@ -89,7 +96,7 @@ public class LoginDialog extends JFrame {
 				}
 			}
 		});
-		loginButton.setBounds(377, 148, 117, 29);
+		loginButton.setBounds(377, 149, 117, 29);
 		contentPane.add(loginButton);
 		
 		JButton CreateAccButton = new JButton("Create new Account");
